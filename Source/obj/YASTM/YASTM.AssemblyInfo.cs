b21e92f8b39b2bf3c2cc7a59a387bfb8ef9e3e0b
Moofoo001 <moofoo001@gmail.com>
2025-10-15 21:02:09 +0200

    V1.4.2

--- Source/obj/YASTM/YASTM.AssemblyInfo.cs
+++ Source/obj/YASTM/YASTM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YASTM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06ce16b9fa10653bd9cd649c9ec35883848f4e6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf7ecd4eff0d1caeb7a7d38ec4ee9ccdf3527a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("YASTM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YASTM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
