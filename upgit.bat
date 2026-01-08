964f91a38e6737bcaf9be74c5a3fa8562d591e07
Moofoo001 <moofoo001@gmail.com>
2026-01-08 23:33:28 +0100

    V1.5.2 bloodwine vat fix and ferengi rank icons

--- upgit.bat
+++ upgit.bat
@@ -3,6 +3,6 @@
 echo "Adding new / changed files"
 git add .\
 echo "commiting my presures ....."
-git commit .\ -m "V1.5.1 career/usage overhaul update"
+git commit .\ -m "V1.5.2 bloodwine vat fix and ferengi rank icons"
 echo "push it to the limit ........."
 git push -u origin rimworld_st_mod
