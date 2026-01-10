ce622b3adcc392885297e21828197d87cd96d8e5
Moofoo001 <moofoo001@gmail.com>
2026-01-10 22:20:23 +0100

    V1.5.4 Career tweaks for Ferengi and Romulan

--- upgit.bat
+++ upgit.bat
@@ -3,6 +3,6 @@
 echo "Adding new / changed files"
 git add .\
 echo "commiting my presures ....."
-git commit .\ -m "V1.5.2 bloodwine vat fix and ferengi rank icons"
+git commit .\ -m "V1.5.4 Career tweaks for Ferengi and Romulan"
 echo "push it to the limit ........."
 git push -u origin rimworld_st_mod
