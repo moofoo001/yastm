38a1f7d636364b2cd3eec253aea66b41cc439297
Moofoo001 <moofoo001@gmail.com>
2026-01-13 12:23:41 +0100

    V1.5.8 - tweaks to building icons and added glow to cloaking field

--- upgit.bat
+++ upgit.bat
@@ -3,6 +3,6 @@
 echo "Adding new / changed files"
 git add .\
 echo "commiting my presures ....."
-git commit .\ -m "V1.5.6 - bug fixes and improvements"
+git commit .\ -m "V1.5.8 - tweaks to building icons and added glow to cloaking field"
 echo "push it to the limit ........."
 git push -u origin rimworld_st_mod
