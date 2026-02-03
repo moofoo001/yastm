5fd18cf234946b088c67185166af40dc912a47b7
moofoo001 <moofoo001@gmail.com>
2026-02-03 14:24:07 +0100

    V1.6.5 polishing mod

--- YASTM/Source/obj/YASTM/YASTM.AssemblyInfo.cs
+++ YASTM/Source/obj/YASTM/YASTM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YASTM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6a658dbc16a99e9a0b051f3ecaf2acd3256d766")]
 [assembly: System.Reflection.AssemblyProductAttribute("YASTM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YASTM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
