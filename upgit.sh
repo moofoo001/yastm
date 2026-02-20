abea6f7da3eeb2ff43126518da967eabce970c5f
moofoo001 <moofoo001@gmail.com>
2026-02-20 11:09:36 +0100

    V1.7.9 Dominion research update

--- upgit.sh
+++ upgit.sh
@@ -2,6 +2,6 @@
 echo 'Adding new / changed files'
 git add ./
 echo 'commiting my presures .....'
-git commit ./ -m "V1.7.8 mod settings , bug fixing, ketracle white production "
+git commit ./ -m "V1.7.9 Dominion research update "
 echo 'push it to the limit .........'
 git push -u origin rimworld_st_mod
