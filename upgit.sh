bd7ce4b314d5bb1bb0f8135f211751c930ae53d9
moofoo001 <moofoo001@gmail.com>
2026-03-10 13:47:07 +0100

    V1.8.9 Tal Shiar suit added

--- upgit.sh
+++ upgit.sh
@@ -2,6 +2,6 @@
 echo 'Adding new / changed files'
 git add ./
 echo 'commiting my presures .....'
-git commit ./ -m "V1.8.8 Jem'Hadar and Vorta cloning fixed"
+git commit ./ -m "V1.8.9 Tal Shiar suit added"
 echo 'push it to the limit .........'
 git push -u origin rimworld_st_mod
